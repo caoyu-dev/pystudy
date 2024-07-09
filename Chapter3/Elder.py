498c87d9af52e264b7d8217b33ce6baade7abd56
caoyu <choyumin01@gmail.com>
2024-07-09 23:31:10 +0900

    feat. 연습문제 7 수정

--- Chapter3/Elder.py
+++ Chapter3/Elder.py
@@ -1,18 +1,28 @@
-#https://dmoj.ca/problem/coci18c4p1v
+#https://dmoj.ca/problem/coci18c4p1
 
 # 초기 마법사를 저장할 변수
-current_wizard = input()[0]
+initial_wizard = input()[0]
+if len(initial_wizard) != 1 or not ('A' <= initial_wizard <= 'Z'):
+    exit("초기 마법사 오류")
 
 # 대결의 수를 저장할 변수
-n = int(input())
+n = input()
+if not n.isdigit() or not (1 <= int(n) <= 100):
+    exit("결투 수 오류")
+n = int(n)
 
 # Elder Wand 를 소유했던 마법사들을 저장할 집합
+current_wizard = initial_wizard
 wizards_set = set()
 wizards_set.add(current_wizard)
 
 # 대결 결과를 처리
 for _ in range(n):
     duel = input()
+    # duel 이 3 이 아닌 경우, 중간이 ' ' 없는 경우, duel 하는 사람이 A 와 Z 사이에 나타나지 않는 경우, duel 하는 사람이 같은 경우
+    if len(duel) != 3 or duel[1] != ' ' or not ('A' <= duel[0] <= 'Z') or not ('A' <= duel[2] <= 'Z') or duel[0] == duel[2]:
+        exit("duel 케이스에 문제가 있다")
+
     winner = duel[0]
     loser = duel[2]
 
